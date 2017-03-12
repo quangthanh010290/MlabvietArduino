e8dcd7d927c03bed5b2045f074ef327f68be987e
quangthanh010290 <quangthanh010290@gmail.com>
2017-03-12 11:59:15 +0700

    update

--- examples/blinky/Release/gpio_if.d
+++ examples/blinky/Release/gpio_if.d
@@ -1,9 +1,9 @@
 # FIXED
 
 gpio_if.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/gpio_if.c
-gpio_if.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h
-gpio_if.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h
-gpio_if.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h
+gpio_if.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdio.h
+gpio_if.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/linkage.h
+gpio_if.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdarg.h
 gpio_if.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_types.h
 gpio_if.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_ints.h
 gpio_if.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_memmap.h
@@ -18,9 +18,9 @@ gpio_if.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/driverlib/rom_patch.h
 gpio_if.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/gpio_if.h
 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/gpio_if.c: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdarg.h: 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_types.h: 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_ints.h: 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_memmap.h: 
--- examples/blinky/Release/main.d
+++ examples/blinky/Release/main.d
@@ -1,9 +1,9 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h
-main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h
-main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdarg.h
 main.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_types.h
 main.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_ints.h
 main.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_memmap.h
@@ -20,9 +20,9 @@ main.obj: C:/ti/CC3200SDK_1.3.0/cc3200-sdk/example/common/gpio_if.h
 main.obj: ../pinmux.h
 
 ../main.c: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.2.LTS/include/stdarg.h: 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_types.h: 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_ints.h: 
 C:/ti/CC3200SDK_1.3.0/cc3200-sdk/inc/hw_memmap.h: 
